e76778b0c7a1dd62e5142e95fb22fc2893df1c92
Tani314 <tanitabassum299@gmail.com>
2020-03-03 16:49:18 -0500

    stock color showing

--- client/components/stock.js
+++ client/components/stock.js
@@ -1,25 +1,21 @@
 import React from "react";
 
 const setStockStatus = stock => {
-  if (stock.openPrice === stock.currPrice / stock.totalQuantity)
-    return "equalToOpen";
-  if (stock.openPrice > stock.currPrice / stock.totalQuantity)
-    return "lessThanOpen";
-  if (stock.openPrice < stock.currPrice / stock.totalQuantity)
-    return "greaterThanOpen";
+  if (stock.openPrice === stock.currPrice) return `equalToOpen`;
+  else if (stock.openPrice > stock.currPrice) return `lessThanOpen`;
+  else return `greaterThanOpen`;
 };
+
 export const Stock = props => {
   const { stock } = props;
   const stockStatus = setStockStatus(stock);
   return (
-    <div className={`line-item ${stockStatus}`}>
+    <div className={`${stockStatus}`}>
       <div className="column">
         {stock.ticker} - {stock.totalQuantity} Shares ${(
           stock.currPrice * stock.totalQuantity
         ).toFixed(2)}
       </div>
-      {/* {stockStatus === 'greaterThanOpen' && <div className="column">↑</div>}
-    {stockStatus === 'lessThanOpen' && <div className="column">↓</div>} */}
     </div>
   );
 };
